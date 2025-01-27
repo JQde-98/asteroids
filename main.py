859b8b16de8fb3e20126c2f4a83d161dca2aaf7d
beeX <beeXgit@gmail.com>
2025-01-27 11:37:50 +0100

    added collisions

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import pygame
 from constants import *
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import *
+from circleshape import CircleShape
+from shot import Shot
 
 def main():
 
@@ -11,8 +15,13 @@ def main():
 
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    shots = pygame.sprite.Group()
 
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (updatable, drawable, asteroids)
+    AsteroidField.containers = (updatable,)
+    Shot.containers = (updatable, drawable, shots)
 
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
@@ -24,6 +33,7 @@ def main():
     y = (SCREEN_HEIGHT / 2)
 
     player = Player(x, y, PLAYER_RADIUS)
+    field = AsteroidField()
 
     ### Game-loop below###
     while True:
@@ -33,13 +43,16 @@ def main():
                 return
 
         screen.fill((0,0,0))
-        player.draw(screen)
 
         dt = (clock.tick(60)) / 1000
         for i in updatable:
             i.update(dt)
+        for i in asteroids:
+            if i.collision(player):
+                print("Game Over!")
+                exit()
         for i in drawable:
-            i.update(dt)
+            i.draw(screen)
 
         pygame.display.flip()
 
